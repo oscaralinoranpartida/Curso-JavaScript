dd71552865acdaf06ea89e17ab62cf9848b807b7
oscaralinoranpartida <127965264+oscaralinoranpartida@users.noreply.github.com>
2023-04-19 00:48:55 -0600

    Add files via upload

---
+++ Object Oriented Programming/8.js
@@ -0,0 +1,8 @@
+function House(numBedrooms) {
+    this.numBedrooms = numBedrooms;
+  }
+  
+  // Add your code below this line
+  let myHouse=new House(5);
+  
+  console.log(myHouse instanceof House)
